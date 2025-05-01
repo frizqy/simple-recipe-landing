ca9c58efbdde6057cc21c64e02c2b1219dc3de04
frizqy <frizqy.mahrizal.362@gmail.com>
2025-05-01 22:57:10 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='w-full h-screen flex justify-center items-center flex-col bg-(--antiflash-white-2)'>
+        <div className='flex gap-5 flex-col justify-center items-center text-center w-[60%] h-[50%]'>
+          <p className='font-(family-name:--playfair-display) font-bold text-4xl'>Simple Recipe Web</p>
+          <p className='font-(family-name:--playfair-display) font-normal text-[15px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt sequi commodi, in at, eligendi, soluta ipsa provident consequuntur dolorem ipsam blanditiis unde vel cum accusantium dolore id. Laboriosam, commodi fugit!</p>
+        </div>
+        <div className='flex justify-center gap-5 items-center text-center'>
+          <div className='flex justify-center flex-col items-center w-[200px] h-[250px] bg-(--antiflash-white-3) rounded-lg shadow-2xl'>
+            <img className='w-[150px] -translate-y-[30px]' src="./1.png" alt="" />
+            <div className='h-[100px]'>
+              <p className='font-(family-name:--playfair-display) font-bold m-2'>Peyek Pake Udang</p>
+              <p className='font-(family-name:--playfair-display) m-2'>Lorem ipsum dolor</p>
+              <div className='size-[50px] bg-amber-400 rounded-2xl float-right'></div>
+            </div>
+          </div>
+          <div className='flex justify-center flex-col items-center w-[200px] h-[250px] bg-(--antiflash-white-3) rounded-lg shadow-2xl'>
+            <img className='w-[150px] -translate-y-[30px]' src="./2.png" alt="" />
+            <div className='h-[100px]'>
+              <p className='font-(family-name:--playfair-display) font-bold m-2'>Rujak Sayur</p>
+              <p className='font-(family-name:--playfair-display) m-2'>Lorem ipsum dolor</p>
+              <div className='size-[50px] bg-amber-400 rounded-2xl float-right'></div>
+            </div>
+          </div>
+          <div className='flex justify-center flex-col items-center w-[200px] h-[250px] bg-(--antiflash-white-3) rounded-lg shadow-2xl'>
+            <img className='w-[150px] -translate-y-[30px]' src="./3.png" alt="" />
+            <div className='h-[100px]'>
+              <p className='font-(family-name:--playfair-display) font-bold m-2'>Lontong Kering</p>
+              <p className='font-(family-name:--playfair-display) m-2'>Lorem ipsum dolor</p>
+              <div className='size-[50px] bg-amber-400 rounded-2xl float-right'></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Navbar.tsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+function Navbar() {
+  return (
+    <div className='w-full p-[20px] fixed font-(family-name:--playfair-display)'>
+        <header className='flex items-center justify-between'>
+            <div className='w-[100px] flex text-center justify-center'>
+                <p className='font-black'>Recipe Web</p>
+            </div>
+            <nav className=' flex items-center justify-center'>
+                <ul className='flex justify-center items-center gap-3'>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Service</a></li>
+                </ul>
+            </nav>
+            <div className='w-[100px] bg-black flex text-center justify-center rounded-2xl'>
+                <button className='text-white'>ContactUs</button>
+            </div>
+        </header>
+    </div>
+  )
+}
+
+export default Navbar
